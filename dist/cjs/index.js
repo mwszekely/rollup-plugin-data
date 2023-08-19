f0b0ceba60168af43b5e0234907c1ce1e0bd7ed9
mwszekely <19625841+mwszekely@users.noreply.github.com>
2023-08-18 22:07:53 -0400

    Don't use base64url, `fetch` doesn't support it

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -225,7 +225,7 @@ export default data;`;
                     case "blob":
                         // encode the data as base64 in both cases.
                         // the virtual module decodes it into a Blob or ArrayBuffer as appropriate
-                        const beforeSplit = rawData.toString("base64url");
+                        const beforeSplit = rawData.toString("base64");
                         const splitSize = 1024; // Must be a multiple of 4, this is 1kb
                         let afterSplit = [];
                         for (let i = 0; i < beforeSplit.length; i += splitSize)
