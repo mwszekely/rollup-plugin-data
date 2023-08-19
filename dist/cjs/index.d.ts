f1a70a5f755eb3fc16235d821690d60e76eb5385
mwszekely <19625841+mwszekely@users.noreply.github.com>
2023-08-18 20:39:30 -0400

    Package fix

---
+++ dist/cjs/index.d.ts
@@ -0,0 +1,103 @@
+/// <reference types="node" />
+import { FilterPattern } from "@rollup/pluginutils";
+import { InputPluginOption } from "rollup";
+type FetchStoreMode = "asset" | "inline" | "url";
+type FetchTypeMode = "json" | "array-buffer" | "text" | "blob";
+interface DataPluginInfo {
+    /** The original import path this asset was imported from (e.g. the string in `import data from "datafile:foo.png"`) */
+    import: string;
+    /** An internal number used to track imports */
+    uniqueId: number;
+    /** The original import where this file can be found, but normalized to a full path (e.g. "foo.png" in `import data from "datafile:foo.png"`, but as a full path) */
+    inputFilePath: string;
+    outputDirectory: string;
+    outputFilePath: string | null;
+    rawData: Buffer | null;
+    fileReferenceId: string | undefined;
+    /**
+     *
+     *
+     * If inline, you get the value synchronously within the same chunk.
+     *
+     * If asset, you get the value asynchronously via `fetch`
+     *
+     */
+    location: FetchStoreMode;
+    /**
+     * |Location|Mode|Result|
+     * |----|----|------|
+     * |Inline|`array-buffer`|Exports a decoded base-64 string|
+     * |Inline|`blob`        |Exports a decoded base-64 string|
+     * |Inline|`json`        |Exports JSON object|
+     * |Inline|`text`        |Exports a string|
+     * |Asset |`array-buffer`|Fetches a file, exports the response's array buffer|
+     * |Asset |`blob`        |Fetches a file, exports the response's blob|
+     * |Asset |`json`        |Fetches a file, exports the response's json|
+     * |Asset |`text`        |Fetches a file, exports the response's text|
+     *
+     */
+    mode: FetchTypeMode;
+    /** Only used for inline locations/modes */
+    mime: string;
+}
+/**
+ * The options that are available on a per-file (or per-extension) basis.
+ */
+export interface PerFileOptions extends Partial<Pick<DataPluginInfo, "location" | "mode" | "mime">> {
+}
+export interface DataPluginOptions {
+    /**
+     * Files prefixed with `datafile:` will always be included, but this can be used to load files even if they don't.
+     */
+    include?: FilterPattern;
+    /**
+     * Excludes any files that were included by `include` (has no effect on `datafile:` ids)
+     */
+    exclude?: FilterPattern;
+    /**
+     * If true, the imports will be the values themselves (instead of promises).
+     *
+     * Use with caution, be sure to check your bundle's output to make sure the promises execute in parallel.
+     */
+    useTopLevelAwait?: boolean;
+    /**
+     * By default, all files are inlined in base64.
+     *
+     * You can use this to cause files to be imported in other ways on a case-by-case basis.
+     *
+     * For example, you can `fetch` and simply return a `Response`, or a `Blob`.
+     */
+    fileOptions?(fullPath: string): PerFileOptions;
+    /**
+     * Choose how files are handled on a per-extension basis.
+     *
+     * Whatever's specified here can be overridden by `preciseFileOptions`, which operates on the entire file path instead of just the file's extension.
+     *
+     * * `location`: Can be `"inline"`, to embed the data in the JS file (as base64 if necessary), or "asset", to be served separately.
+     * * `mode`: What does the import return? Can be `"array-buffer"`, `"blob"`, `"json"`, or `"text"`.
+     * * `mime`: `location: "inline"` and `mode: "base"` require a MIME type. Defaults to "application/octet-stream".
+     */
+    fileTypes: Record<`.${string}`, PerFileOptions>;
+    /**
+     * Given the full path of the imported asset, this must return a non-absolute non-relative path, like "assets/banner.png"
+     * that will be relative to the output directory.
+     *
+     * Default is the path relative to the importer, which may become weird if ".." paths are used and dumps everything in the output's root folder.
+     */
+    transformFilePath?(info: TransformFilePathInfo): string;
+}
+export interface TransformFilePathInfo {
+    /** The full path from the root of your local disk */
+    /** The path relative to the project root */
+    pathRelative: string;
+    /** The name of the file (without the extension) */
+    fileName: string;
+    /** The file's extension (including the period at the star) */
+    fileExtWithDot: string;
+    /** A string of the hash of `pathRelative` (not the file's contents, as it may not be available.) */
+    hashPathRelative: string;
+}
+export declare function getDefaultAssetPathInfo(fullFilePath: string, projectRootDir: string): TransformFilePathInfo;
+export declare function getDefaultAssetPath({ fileExtWithDot, fileName }: TransformFilePathInfo): string;
+export default function dataPlugin({ fileOptions, transformFilePath, fileTypes, useTopLevelAwait, exclude, include }?: Partial<DataPluginOptions>): InputPluginOption;
+export { dataPlugin };
---
+++ dist/cjs/index.js
@@ -0,0 +1,284 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var pluginutils = require('@rollup/pluginutils');
+var crypto = require('crypto');
+var promises = require('fs/promises');
+var MagicString = require('magic-string');
+var path = require('path');
+
+const DATA_PREFIX = "datafile:";
+/** When we handle a datafile: id, ensure others don't handle it. The rest of the ID is stripped and replaced with a unique number. */
+const SELFISH_DATA_PREFIX = "\0datafile:";
+/** This is the virtual helper file whose functions decode base64 and Responses into Blobs and strings and such. */
+const DATA_HELPER_DECODE = "\0DATA_HELPER_ENCODE";
+let _il = 0;
+function getDefaultAssetPathInfo(fullFilePath, projectRootDir) {
+    const p = fullFilePath;
+    const bn = path.basename(p);
+    const ext = path.extname(p);
+    const filename = bn.substring(0, bn.length - ext.length);
+    const hasher = crypto.createHash("sha256");
+    const pathRelativeToProject = path.relative(projectRootDir, p);
+    hasher.update(pathRelativeToProject);
+    const hash = hasher.digest("hex");
+    return { fileExtWithDot: ext, fileName: filename, hashPathRelative: hash, pathRelative: pathRelativeToProject };
+}
+function getDefaultAssetPath({ fileExtWithDot, fileName }) {
+    return `assets/${fileName}${fileExtWithDot}`;
+}
+function mergeThing(target, modifier) {
+    return {
+        location: (target === null || target === void 0 ? void 0 : target.location) || (modifier === null || modifier === void 0 ? void 0 : modifier.location),
+        mode: (target === null || target === void 0 ? void 0 : target.mode) || (modifier === null || modifier === void 0 ? void 0 : modifier.mode),
+        mime: (target === null || target === void 0 ? void 0 : target.mime) || (modifier === null || modifier === void 0 ? void 0 : modifier.mime)
+    };
+}
+// Doesn't work on non-BMP characters, if that ever comes up
+function capitalize(str) { return `${(str[0]).toUpperCase()}${str.substring(1)}`; }
+const PLUGIN_NAME = "rollup-plugin-datafile";
+function dataPlugin({ fileOptions, transformFilePath, fileTypes, useTopLevelAwait, exclude, include } = {}) {
+    const filter = pluginutils.createFilter(include, exclude);
+    fileTypes || (fileTypes = {});
+    // source files
+    fileTypes[".css"] = mergeThing(fileTypes[".css"], { mime: "text/css", mode: "text" });
+    fileTypes[".json"] = mergeThing(fileTypes[".json"], { mime: "text/json", mode: "json" });
+    fileTypes[".html"] = mergeThing(fileTypes[".html"], { mime: "text/html", mode: "text" });
+    fileTypes[".js"] = mergeThing(fileTypes[".js"], { mime: "text/javascript", mode: "text" });
+    fileTypes[".mjs"] = mergeThing(fileTypes[".mjs"], { mime: "text/javascript", mode: "text" });
+    fileTypes[".svg"] = mergeThing(fileTypes[".svg"], { mime: "image/svg+xml", mode: "text" });
+    fileTypes[".csv"] = mergeThing(fileTypes[".csv"], { mime: "text/csv", mode: "text" });
+    // raster images
+    fileTypes[".webp"] = mergeThing(fileTypes[".webp"], { mime: "image/webp", mode: "blob" });
+    fileTypes[".gif"] = mergeThing(fileTypes[".gif"], { mime: "image/gif", mode: "blob" });
+    fileTypes[".jpg"] = mergeThing(fileTypes[".jpg"], { mime: "image/jpeg", mode: "blob" });
+    fileTypes[".jpeg"] = mergeThing(fileTypes[".jpeg"], { mime: "image/jpeg", mode: "blob" });
+    fileTypes[".png"] = mergeThing(fileTypes[".png"], { mime: "image/png", mode: "blob" });
+    fileTypes[".ico"] = mergeThing(fileTypes[".ico"], { mime: "image/vnd.microsoft.icon", mode: "blob" });
+    // audio files
+    fileTypes[".mp3"] = mergeThing(fileTypes[".mp3"], { mime: "audio/mpeg", mode: "blob" });
+    fileTypes[".mp4"] = mergeThing(fileTypes[".mp4"], { mime: "audio/mp4", mode: "blob" });
+    fileTypes[".mpeg"] = mergeThing(fileTypes[".mpeg"], { mime: "audio/mpeg", mode: "blob" });
+    fileTypes[".ogg"] = mergeThing(fileTypes[".ogg"], { mime: "application/ogg", mode: "blob" });
+    fileTypes[".webm"] = mergeThing(fileTypes[".webm"], { mime: "audio/webm", mode: "blob" });
+    // fonts
+    fileTypes[".otf"] = mergeThing(fileTypes[".otf"], { mime: "font/otf", mode: "blob" });
+    fileTypes[".ttf"] = mergeThing(fileTypes[".ttf"], { mime: "font/ttf", mode: "blob" });
+    fileTypes[".woff"] = mergeThing(fileTypes[".woff"], { mime: "font/woff", mode: "blob" });
+    fileTypes[".woff2"] = mergeThing(fileTypes[".woff2"], { mime: "font/woff2", mode: "blob" });
+    // document types
+    fileTypes[".txt"] = mergeThing(fileTypes[".txt"], { mime: "text/plain", mode: "text" });
+    fileTypes[".docx"] = mergeThing(fileTypes[".docx"], { mime: "application/vnd.openxmlformats-officedocument.wordprocessingml.document", mode: "blob" });
+    fileTypes[".xlsx"] = mergeThing(fileTypes[".xlsx"], { mime: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", mode: "blob" });
+    fileTypes[".pptx"] = mergeThing(fileTypes[".pptx"], { mime: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", mode: "blob" });
+    fileTypes[".doc"] = mergeThing(fileTypes[".doc"], { mime: "application/msword", mode: "blob" });
+    fileTypes[".xls"] = mergeThing(fileTypes[".xls"], { mime: "application/vnd.ms-excel", mode: "blob" });
+    fileTypes[".ppt"] = mergeThing(fileTypes[".ppt"], { mime: "application/vnd.ms-powerpoint", mode: "blob" });
+    fileTypes[".pdf"] = mergeThing(fileTypes[".pdf"], { mime: "application/pdf", mode: "blob" });
+    const filePathsToEmitIds = new Map();
+    const promisesToWaitFor = new Set();
+    let projectDir = process.cwd();
+    let infoByImport = new Map();
+    let infoByUid = new Map();
+    return {
+        name: PLUGIN_NAME,
+        async resolveId(id, importer, { assertions }) {
+            var _a;
+            if (id == DATA_HELPER_DECODE)
+                return id;
+            if (id.startsWith(DATA_PREFIX) || (!!include && filter(id))) {
+                const url = new URL(id);
+                const searchParams = url.searchParams;
+                let importerDir = importer ? path.dirname(importer) : projectDir;
+                let inputFilePath = url.pathname.startsWith("~/") ? path.join(projectDir, url.pathname.substring(2)) :
+                    url.pathname.startsWith("~") ? path.join(projectDir, url.pathname.substring(1)) :
+                        path.isAbsolute(url.pathname) ? url.pathname :
+                            path.join(importerDir, url.pathname);
+                if (!infoByImport.has(id)) {
+                    let outputDirectory = ".";
+                    let outputFilePath;
+                    let ext;
+                    {
+                        const p = inputFilePath;
+                        const bn = path.basename(p);
+                        ext = path.extname(p);
+                        const filename = bn.substring(0, bn.length - ext.length);
+                        const hasher = crypto.createHash("sha256");
+                        const pathRelativeToProject = path.relative(projectDir, p);
+                        hasher.update(pathRelativeToProject);
+                        const hash = hasher.digest("hex");
+                        const o = { fileExtWithDot: ext, fileName: filename, hashPathRelative: hash, pathRelative: pathRelativeToProject };
+                        if (transformFilePath)
+                            outputFilePath = transformFilePath(o);
+                        else
+                            outputFilePath = getDefaultAssetPath(o);
+                    }
+                    let fileReferenceId;
+                    let { mode: defaultMode, location: defaultLocation, mime: defaultMime } = mergeThing((_a = fileTypes === null || fileTypes === void 0 ? void 0 : fileTypes[ext]) !== null && _a !== void 0 ? _a : {}, (fileOptions !== null && fileOptions !== void 0 ? fileOptions : (() => ({})))(inputFilePath));
+                    const { location: aLocation, mime: aMime, mode: aMode } = (assertions || {});
+                    const [qLocation, qMime, qMode] = [searchParams.get("location"), searchParams.get("mime"), searchParams.get("mode")];
+                    // Input validation, yay
+                    if (aLocation && qLocation && aLocation != qLocation)
+                        throw new Error(`${importer} imported ${id}, but specified ${aLocation} via its import assertion.`);
+                    if (aMime && qMime && aMime != qMime)
+                        throw new Error(`${importer} imported ${id}, but specified ${aMime} via its import assertion.`);
+                    if (aMode && qMode && aMode != qMode)
+                        throw new Error(`${importer} imported ${id}, but specified ${aMode} via its import assertion.`);
+                    const location = qLocation || aLocation || defaultLocation || "inline";
+                    const mode = (qMode || aMode || defaultMode || "blob");
+                    const mime = (qMime || aMime || defaultMime || "application/octet-stream");
+                    // Back to input validation...
+                    if (location != "asset" && location != "inline" && location != "url")
+                        throw new Error(`${importer} imported ${id} with an unknown location specified: "${location}"`);
+                    if (mode != "blob" && mode != "array-buffer" && mode != "json" && mode != "text")
+                        throw new Error(`${importer} imported ${id} with an unknown mode specified: "${mode}"`);
+                    if (location == "asset" || location == "url") {
+                        fileReferenceId = this.emitFile({ type: "asset", fileName: outputFilePath });
+                        filePathsToEmitIds.set(outputFilePath, fileReferenceId);
+                    }
+                    let newInfo = {
+                        outputFilePath: fileReferenceId ? this.getFileName(fileReferenceId) : outputFilePath,
+                        mode,
+                        rawData: null,
+                        uniqueId: _il++,
+                        inputFilePath,
+                        outputDirectory,
+                        fileReferenceId,
+                        import: id,
+                        location,
+                        mime
+                    };
+                    infoByImport.set(newInfo.import, newInfo);
+                    infoByUid.set(newInfo.uniqueId, newInfo);
+                }
+                return {
+                    id: `${SELFISH_DATA_PREFIX}${infoByImport.get(id).uniqueId}`
+                };
+            }
+        },
+        async load(id) {
+            if (id == DATA_HELPER_DECODE) {
+                return decodeResponseHelperFile;
+            }
+            else if (id.startsWith(SELFISH_DATA_PREFIX)) {
+                let info = infoByUid.get(+id.substring(SELFISH_DATA_PREFIX.length));
+                let m = capitalize(info.mode);
+                if (m == "Array-buffer")
+                    m = "ArrayBuffer";
+                if (info.location == "asset") {
+                    return `
+import { decodeAsset${m} } from ${JSON.stringify(DATA_HELPER_DECODE)};
+const data = ${useTopLevelAwait ? "await " : ""}decodeAsset${m}(fetch(${JSON.stringify(path.relative(info.outputDirectory, info.outputFilePath))}));
+export default data;`;
+                }
+                else if (info.location == "url") {
+                    return `
+const data = ${JSON.stringify(path.relative(info.outputDirectory, info.outputFilePath))};
+export default data;`;
+                }
+                else {
+                    return `
+import { decodeInline${m} } from ${JSON.stringify(DATA_HELPER_DECODE)};
+const data = ${useTopLevelAwait ? "await " : ""}decodeInline${m}(undefined/**@__AWAITING_DATAFILE_BASE64_${info.uniqueId}__**/);
+export default data;`;
+                }
+            }
+        },
+        async buildEnd() {
+            // If other plugins write files during buildEnd, wait for them to do so before reading what they've written.
+            // TODO: There's gotta be a better way than asking those plugins to look for us and tell us what they're doing, right.
+            await Promise.all([...promisesToWaitFor]);
+            // Now read all of the files' data (they should all exist by now).
+            await Promise.all([...infoByImport].map(async ([, info]) => { info.rawData = await promises.readFile(info.inputFilePath); }));
+            // Read all the files we've been emitting files for, and wait for all of them to finish in parallel.
+            for (const [_id, info] of infoByImport) {
+                if (info.location == "asset" || info.location == "url") {
+                    this.setAssetSource(info.fileReferenceId, info.rawData);
+                }
+            }
+        },
+        async renderChunk(_code, _chunk, _options) {
+            // We need to replace the temporary variables we set during load, but, like,
+            // is this...the best way of doing this...?
+            // I haven't found a better way to "replace" a variable's value after build,
+            // and this works, but it's pretty sus.
+            const s = new MagicString(_code);
+            s.replaceAll(/undefined\/\*\*@__AWAITING_DATAFILE_BASE64_([0-9]+)__\*\*\//g, (_m, i) => {
+                var _a;
+                const uniqueId = +i;
+                const info = infoByUid.get(uniqueId);
+                if (!info)
+                    return "undefined";
+                const rawData = info.rawData;
+                switch (info.mode) {
+                    case "text":
+                        // return the data, assume it's encoded in UTF-8 (TODO on that)
+                        return JSON.stringify((_a = info.rawData) === null || _a === void 0 ? void 0 : _a.toString("utf-8"));
+                    case "json":
+                        // stringify then parse to make sure it's actually JSON, but not parsed from a string on the client at runtime
+                        // (lots of unnecessary quotes in the result but a minifier'll clean those right out)
+                        return info.rawData ? JSON.parse(JSON.stringify(info.rawData.toString("utf-8"))) : "undefined";
+                    case "array-buffer":
+                    case "blob":
+                        // encode the data as base64 in both cases.
+                        // the virtual module decodes it into a Blob or ArrayBuffer as appropriate
+                        const beforeSplit = rawData.toString("base64url");
+                        const splitSize = 120;
+                        let afterSplit = [];
+                        for (let i = 0; i < beforeSplit.length; i += splitSize)
+                            afterSplit.push(beforeSplit.substring(i, i + splitSize));
+                        return `"data:${info.mime};base64,\\\n${afterSplit.join("\\\n")}"`;
+                    default:
+                        // why would you do this?
+                        throw new Error(`Unknown mode for file "${info.inputFilePath}": ${info.mode}`);
+                }
+            });
+            return {
+                code: s.toString(),
+                map: s.generateMap({ hires: true }), // ??? https://stackoverflow.com/questions/76186660/how-to-use-magicstring-to-provide-a-sourcemap-with-rollups-renderchunk-hook
+            };
+        },
+        api: {
+            promisesToWaitFor,
+            filePathsToEmitIds
+        }
+    };
+}
+// TODO: Make this not be a string, that's not awesome.
+// Also TODO a couple of these are basically no-ops, which is lame.
+const decodeResponseHelperFile = `
+
+export async function decodeInlineBlob(base64)        { return (await fetch(base64)).blob(); }
+export async function decodeInlineArrayBuffer(base64) { return (await fetch(base64)).arrayBuffer(); }
+export async function decodeInlineText(text)          { return (await Promise.resolve(text)); }
+export async function decodeInlineJson(json)          { return (await Promise.resolve(json)); }
+export async function decodeInlineResponse(base64)    { return (await fetch(base64)); }
+
+export async function decodeAssetBlob(response, backupValue = null)      { return await decodeAssetShared(response, r => r.blob(), backupValue); }
+export async function decodeAssetText(response, backupValue = "")        { return await decodeAssetShared(response, r => r.text(), backupValue); }
+export async function decodeAssetJson(response, backupValue = "")        { return await decodeAssetShared(response, r => r.json(), backupValue); }
+export async function decodeAssetArrayBuffer(response, backupValue = "") { return await decodeAssetShared(response, r => r.arrayBuffer(), backupValue); }
+export async function decodeAssetResponse(response)                      { return await decodeAssetShared(response, r => r, response); }
+
+async function decodeAssetShared(response, action, backup) {
+	if ("then" in response) {
+        return await decodeAssetShared(await response, action, backup);
+    }
+    if (response.ok) {
+        return await action(await response);
+    }
+    if (backup != null)
+        return await backup;
+
+    throw new Error("Critical error: could not load file: HTTP response " + response.status);
+}
+
+`;
+
+exports.dataPlugin = dataPlugin;
+exports.default = dataPlugin;
+exports.getDefaultAssetPath = getDefaultAssetPath;
+exports.getDefaultAssetPathInfo = getDefaultAssetPathInfo;
+module.exports = Object.assign(exports.default, exports);
+//# sourceMappingURL=index.js.map
